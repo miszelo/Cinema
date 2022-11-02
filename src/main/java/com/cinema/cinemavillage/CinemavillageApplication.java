2647cff80668d61d405f864c7cb7cc59a9e71ec5
michal <michal.kawczak@gmail.com>
2022-11-02 23:03:18 +0100

    creating project

---
+++ src/main/java/com/cinema/cinemavillage/CinemavillageApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.cinemavillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemavillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemavillageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinema/cinemavillage/CinemavillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.cinemavillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemavillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
