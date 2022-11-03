807e3cea9d492c8c7df3a26468a9936e3349a574
MikosJ <56031072+MikosJ@users.noreply.github.com>
2022-11-03 20:47:52 +0100

    Added web and api controller classes, w/o any endpoints

---
+++ src/main/java/com/cinema/cinemavillage/controller/ApiController.java
@@ -0,0 +1,9 @@
+package com.cinema.cinemavillage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+}
---
+++ src/main/java/com/cinema/cinemavillage/controller/WebController.java
@@ -0,0 +1,7 @@
+package com.cinema.cinemavillage.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+}
