419787e2455c74955b4317dbf84efd411dcc4288
MikosJ <56031072+MikosJ@users.noreply.github.com>
2022-11-03 20:42:29 +0100

    Added web and api controllers, without any endpoints

---
+++ src/main/java/com/cinemavillage/controller/WebController.java
@@ -0,0 +1,10 @@
+package com.cinemavillage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+    
+}
