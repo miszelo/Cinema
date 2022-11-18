704755982b73cc1ad3f25da01829888fa94afc0d
MikosJ <56031072+MikosJ@users.noreply.github.com>
2022-11-18 19:56:44 +0100

    Added movie class

---
+++ src/main/java/com/cinemavillage/model/movie/Movie.java
@@ -0,0 +1,21 @@
+package com.cinemavillage.model.movie;
+
+import com.cinemavillage.model.hall.Hall;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+public class Movie {
+
+    @Id
+    String id;
+
+    String title;
+
+    int duration;
+
+    List<Hall> seatState;
+
+}
