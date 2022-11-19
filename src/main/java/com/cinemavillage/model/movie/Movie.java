510b2aa3f0e4727112ee1650796968cc55e3531d
MikosJ <56031072+MikosJ@users.noreply.github.com>
2022-11-19 13:33:15 +0100

    displaying movies on homepage

--- src/main/java/com/cinemavillage/model/movie/Movie.java
+++ src/main/java/com/cinemavillage/model/movie/Movie.java
@@ -1,13 +1,16 @@
 package com.cinemavillage.model.movie;
 
 import com.cinemavillage.model.hall.Hall;
+import com.fasterxml.jackson.annotation.JsonKey;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import javax.management.DescriptorKey;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,6 +18,7 @@ import java.util.List;
 @Data
 public class Movie {
 
+    @Field("movieId")
     @Id
     String id;
 
@@ -26,14 +30,4 @@ public class Movie {
 
     LocalDateTime dateOfScreening;
 
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", duration=" + duration +
-                ", seatState=" + seatState +
-                ", dateOfScreening=" + dateOfScreening +
-                '}';
-    }
 }
