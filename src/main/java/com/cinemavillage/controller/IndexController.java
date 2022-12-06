b88180f1a34a6620c79767a56c7cba8a5902c9dd
michal <michal.kawczak@gmail.com>
2022-12-06 23:05:42 +0100

    change in /home

--- src/main/java/com/cinemavillage/controller/IndexController.java
+++ src/main/java/com/cinemavillage/controller/IndexController.java
@@ -4,12 +4,10 @@ import com.cinemavillage.model.Movie;
 import com.cinemavillage.repository.HallRepository;
 import com.cinemavillage.repository.MovieRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,25 +19,12 @@ import java.util.Optional;
 @Controller
 @AllArgsConstructor
 public class IndexController {
-
-    private final static String HOME_PAGE = "home_page";
-    private final static String HOME_PAGE_LOGGED = "home_page_logged";
+    private final static String HOME_PAGE = "homePage";
     private final static String CINEMA = "cinemaHallLayout";
 
     private final HallRepository hallRepository;
 
     private final MovieRepository movieRepository;
-
-    @RequestMapping("/")
-    public ModelAndView homePage() {
-        return new ModelAndView(HOME_PAGE);
-    }
-//    @RequestMapping("/")
-//    public ModelAndView homePagedffd() {
-//        List<?> s  = hallRepository.getAll();
-//        ModelAndView modelAndView = new ModelAndView(HOME_PAGE);
-//        modelAndView.addObject(s);
-//    }
 
     @RequestMapping("/home/{date}")
     public String homePageLogged(Model model, @PathVariable Optional<String> date) {
@@ -55,7 +40,7 @@ public class IndexController {
         //TODO
         //do modelu dodać coś na podstawie czego można mieć godziny seansów, tj tytuł filmu i date
         //czyli dla kazdego tytulu filmu i danej daty znalezc godziny seansów ktore powinny zostac zawarte w modelu
-        return HOME_PAGE_LOGGED;
+        return HOME_PAGE;
     }
 
     @RequestMapping("/cinema")
