25051f1de1c581f4038c42a11d8ee9cd9ea98898
MikosJ <56031072+MikosJ@users.noreply.github.com>
2022-12-06 21:52:26 +0100

    movie and hall documents

--- src/main/java/com/cinemavillage/model/hall/Hall.java
+++ src/main/java/com/cinemavillage/model/hall/Hall.java
@@ -4,6 +4,7 @@ import com.cinemavillage.model.movie.Movie;
 import com.cinemavillage.model.seat.Seat;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -11,9 +12,16 @@ import java.util.ArrayList;
 @Document
 public class Hall {
     @Id
+    @Field(name = "hallID")
     private Long id;
+
+    @Field(name = "hallSeatState")
     private ArrayList<Seat> seatState;
+
+    @Field(name = "hallScreeningTime")
     private LocalDateTime screeningTime;
+
+    @Field(name = "hallMovieName")
     private Movie movie;
 
 }
--- src/main/java/com/cinemavillage/model/movie/Movie.java
+++ src/main/java/com/cinemavillage/model/movie/Movie.java
@@ -3,18 +3,23 @@ package com.cinemavillage.model.movie;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
 @Data
 public class Movie {
 
     @Id
+    @Field("movieID")
     String id;
 
+    @Field("movieTitle")
     String title;
 
+    @Field("movieDuration")
     int duration;
 
+    @Field("movieDescription")
     String description;
 
 }
