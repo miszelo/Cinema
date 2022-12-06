033a285fcacde9e29628bc7f1ac906052b9f3283
MikosJ <56031072+MikosJ@users.noreply.github.com>
2022-12-06 22:57:51 +0100

    DATE IndexController

--- src/main/java/com/cinemavillage/controller/IndexController.java
+++ src/main/java/com/cinemavillage/controller/IndexController.java
@@ -4,14 +4,19 @@ import com.cinemavillage.model.Movie;
 import com.cinemavillage.repository.HallRepository;
 import com.cinemavillage.repository.MovieRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -36,12 +41,20 @@ public class IndexController {
 //        modelAndView.addObject(s);
 //    }
 
-    @RequestMapping("/home")
-    public String homePageLogged(Model model) {
-
+    @RequestMapping("/home/{date}")
+    public String homePageLogged(Model model, @PathVariable Optional<String> date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+        if (date.isPresent()) {
+            LocalDate localDate = LocalDate.parse(date.toString() ,formatter);
+        } else {
+            LocalDate localDate = LocalDate.now();
+        }
         List<Movie> movies = movieRepository.findAll();
         model.addAttribute("movies", movies);
         model.addAttribute("today", LocalDateTime.now().truncatedTo(ChronoUnit.DAYS));
+        //TODO
+        //do modelu dodać coś na podstawie czego można mieć godziny seansów, tj tytuł filmu i date
+        //czyli dla kazdego tytulu filmu i danej daty znalezc godziny seansów ktore powinny zostac zawarte w modelu
         return HOME_PAGE_LOGGED;
     }
 
