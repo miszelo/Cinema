261ea8c5ce49c9bb8dec22ba6cee94e1fd95a57f
michal <michal.kawczak@gmail.com>
2022-12-07 21:57:46 +0100

    added user controller and user service

---
+++ src/main/java/com/cinemavillage/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.cinemavillage.controller;
+
+
+import com.cinemavillage.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
