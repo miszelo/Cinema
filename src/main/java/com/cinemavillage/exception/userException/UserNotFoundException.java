f3636517b607dc0d6f6f72f5394df95dc0d92e5d
michal <michal.kawczak@gmail.com>
2022-12-07 22:51:13 +0100

    created UserNotFoundException

---
+++ src/main/java/com/cinemavillage/exception/userException/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.cinemavillage.exception.userException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found!")
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super();
+    }
+}
