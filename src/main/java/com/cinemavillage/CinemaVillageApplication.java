3d9ebd2eed00041cad59691092a1af5066005e5b
michal <michal.kawczak@gmail.com>
2023-01-10 19:35:56 +0100

    changes to deploy obn heroku

--- src/main/java/com/cinemavillage/CinemaVillageApplication.java
+++ src/main/java/com/cinemavillage/CinemaVillageApplication.java
@@ -2,9 +2,10 @@ package com.cinemavillage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CinemaVillageApplication {
+public class CinemaVillageApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaVillageApplication.class, args);
