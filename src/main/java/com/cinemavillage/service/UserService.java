b6bca2f2ce1e8156edcd4e020abfd5d3a92d42e7
michal <michal.kawczak@gmail.com>
2023-01-10 23:39:36 +0100

    added UserService and UserController

---
+++ src/main/java/com/cinemavillage/service/UserService.java
@@ -0,0 +1,21 @@
+package com.cinemavillage.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    public ResponseEntity<?> getTickets() {
+        return null;
+    }
+
+    public ResponseEntity<?> updatePassword() {
+        return null;
+    }
+
+    public ResponseEntity<?> deleteTicket() {
+        return null;
+    }
+}
