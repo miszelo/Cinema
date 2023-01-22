7f12c86855714d6c3d90413bc6cea5281f9172b3
michal <michal.kawczak@gmail.com>
2023-01-22 16:58:20 +0100

    trying to deploy on heroku

--- src/main/java/com/cinemavillage/CinemaVillageApplication.java
+++ src/main/java/com/cinemavillage/CinemaVillageApplication.java
@@ -2,21 +2,16 @@ package com.cinemavillage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Cinema simulator, users can book the tickets for the movie they pick via button with screening hour on it. Then they pick the seats.
+ *
  * @author Mikos Jakub, Kawczak Michał
  * @see SpringApplication
  */
 @SpringBootApplication
-public class CinemaVillageApplication extends SpringBootServletInitializer {
+public class CinemaVillageApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CinemaVillageApplication.class);
-    }
     public static void main(String[] args) {
         SpringApplication.run(CinemaVillageApplication.class, args);
 
